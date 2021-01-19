443cc7fbe8d4ddfd6f9b3d1125fb5c8e6fdc3f5a
Ramiro Melero <72161252+nuevo-rama@users.noreply.github.com>
2021-01-19 11:28:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname,'./public')));
 
 app.get('/',(req,res)=>{
 
-  res.sendFile(path.join(__dirname,'./views/home.html'))
+  res.sendFile(path.join(__dirname,'./views/index.html'))
 });
 
 app.get('/login',(req,res)=> {
